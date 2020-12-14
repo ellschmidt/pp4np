627eb925d2bbf73b126193779660c5c807c893ad
Ellen <ellschmidt@uos.de>
2020-12-14 20:42:25 +0100

    Most Basic Structre 1.0
    First thoughts on structure

---
+++ head.py
@@ -0,0 +1,58 @@
+# import modules
+
+# load vocab file
+# read dictionary file
+# check for new entries
+# add them to vocab file while creating entry-objects
+
+# "Begrüßung"
+# "Main Menü"
+
+'''
+while input != "Exit!":
+	
+	while input != "out":
+		
+		if input == "Dic":
+			
+			translate()
+			
+			show.main_menu()
+			
+		elif input == "Vocab":
+			
+			vocab()
+			
+			show.main_menu()
+            
+        elif input == "Grammar":
+            
+            grammar()
+            
+            show.main_menu()
+            
+        elif input == "MadLib":
+            
+            mad()
+            
+            show.main_menu()
+            
+        elif input == "Stats":
+        
+            stats()
+            
+            show.main_menu()
+            
+        elif input == "Help":
+            
+            print(help)
+            
+            show.main_menu()
+          
+        else:
+            Error
+'''
+
+#write to VocabFile
+
+#"Bye"
